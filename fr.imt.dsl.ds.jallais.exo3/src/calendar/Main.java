e9f6cc98a8436bc3df48c2938918c354529a264e
Adrien JALLAIS <adrien.jallais@protonmail.com>
2022-11-28 16:17:51 +0100

    doc: add class diagram from ecore

--- fr.imt.dsl.ds.jallais.exo3/src/calendar/Main.java
+++ fr.imt.dsl.ds.jallais.exo3/src/calendar/Main.java
@@ -3,34 +3,11 @@ package calendar;
 public class Main {
 	
 	public static void main(String[] args) {
-		
-		System.out.println("Output of ugly method:");
-		System.out.println(buildCalendarUgly());
-		System.out.println("\n");
 		System.out.println("Output of beautiful method:");
 		System.out.println(buildGUIBeautiful());
 
 	}
-	
-	private static Frame buildCalendarUgly(){
-		Frame frame = new Frame();
-		Section event1 = new Section();
-		event1.setName("DSL tutorial");
-		event1.setDate(2009, 11, 8);
-		event1.setStartTime("09:00");
-		event1.setEndTime("16:00");
-		event1.setLocation("Aarhus Music Hall");
-		frame.addEvent(event1);
-		Section event2 = new Section();
-		event2.setName("Making use of Patterns");
-		event2.setLocation("Aarhus Music Hall");
-		event2.setStartTime("14:15");
-		event2.setEndTime("15:45");
-		event2.setDate(2009, 10, 5);
-		frame.addEvent(event1);
-		return frame;
-	}
-	
+
 	private static Frame buildGUIBeautiful(){
 		
 		FrameBuilder frameBuilder = new FrameBuilder();
